d42128e1659ed849d457b48e51bf838ffcb48507
Lucas Henrique <validacao01.inobag@gmail.com>
2022-05-24 12:53:38 -0300

    -Iniciar contagem em segundos ao pressionar start;

--- src/components/FormulárioPrincipal.vue
+++ src/components/FormulárioPrincipal.vue
@@ -17,17 +17,17 @@
             />
           </div>
           <section class="m-2">
-            <strong>00:00:00</strong>
+            <strong>{{tempoDecorrido}}</strong>
           </section>
-          <button class="button mr-2">
+          <button class="button mr-2" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
             </span>
             <span>Play</span>
           </button>
-          <button class="button ml-2">
+          <button class="button ml-2" @click="finalizar">
             <span class="icon">
-              <i class="fas fa-pause"></i>
+              <i class="fas fa-stop"></i>
             </span>
             <span>Stop</span>
           </button>
@@ -42,5 +42,27 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "FormulárioPrincipal",
+  data () {
+    return {
+      tempoEmSegundos: 0
+    };
+  },
+  computed: {
+    tempoDecorrido () : string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19);
+    }
+  },
+  methods: {
+    iniciar () {
+      setInterval(()=> {
+        this.tempoEmSegundos+=1;
+        console.log('tempoEmSegundos', this.tempoEmSegundos);
+      }, 1000)
+      console.log('iniciando Cronometro');
+    },
+    finalizar () {
+      console.log('finalizando Cronometro');
+    }
+  }
 });
 </script>
